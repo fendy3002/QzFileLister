c94fc24030f56b5b133d726065ee2af3e72bb2b6
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2018-06-13 18:20:39 +0700

    compile

--- dist/reader/index.js
+++ dist/reader/index.js
@@ -30,62 +30,64 @@ var Service = function Service() {
         log = (0, _log2.default)({});
     }
     return function (pathArg, callback) {
-        var absolutePath = pathArg;
-        if (!_path2.default.isAbsolute(pathArg)) {
-            _path2.default.resolve(pathArg);
-        }
+        return function (resolve, reject) {
+            var absolutePath = pathArg;
+            if (!_path2.default.isAbsolute(pathArg)) {
+                _path2.default.resolve(pathArg);
+            }
 
-        log("Processing for:" + absolutePath);
+            log("Processing for:" + absolutePath);
 
-        var processPath = function processPath(pathArg, tag) {
-            return new Promise(function (resolve, reject) {
-                _fs2.default.lstat(pathArg, function (err, stats) {
-                    if (err) {
-                        log(err);
-                    } else if (stats.isDirectory()) {
-                        trace(pathArg, tag).then(function (result) {
-                            resolve(result);
-                        });
-                    } else {
-                        resolve([{
-                            tag: tag,
-                            path: pathArg,
-                            ext: _path2.default.extname(pathArg),
-                            size: (stats.size / 1024).toFixed(2)
-                        }]);
-                    }
-                });
-            });
-        };
-
-        var trace = function trace(pathArg, tag) {
-            return new Promise(function (resolve, reject) {
-                var promises = [];
-                _fs2.default.readdir(pathArg, function (err, files) {
-                    for (var i = 0; i < files.length; i++) {
-                        var file = files[i];
-                        var filepath = _path2.default.join(pathArg, file);
-                        var newTag = tag.concat([file]);
-
-                        promises.push(processPath(filepath, newTag));
-                    }
-                    Promise.all(promises).then(function (result) {
-                        resolve([].concat.apply([], result));
+            var processPath = function processPath(pathArg, tag) {
+                return new Promise(function (resolve, reject) {
+                    _fs2.default.lstat(pathArg, function (err, stats) {
+                        if (err) {
+                            log(err);
+                        } else if (stats.isDirectory()) {
+                            trace(pathArg, tag).then(function (result) {
+                                resolve(result);
+                            });
+                        } else {
+                            resolve([{
+                                tag: tag,
+                                path: pathArg,
+                                ext: _path2.default.extname(pathArg),
+                                size: (stats.size / 1024).toFixed(2)
+                            }]);
+                        }
                     });
                 });
+            };
+
+            var trace = function trace(pathArg, tag) {
+                return new Promise(function (resolve, reject) {
+                    var promises = [];
+                    _fs2.default.readdir(pathArg, function (err, files) {
+                        for (var i = 0; i < files.length; i++) {
+                            var file = files[i];
+                            var filepath = _path2.default.join(pathArg, file);
+                            var newTag = tag.concat([file]);
+
+                            promises.push(processPath(filepath, newTag));
+                        }
+                        Promise.all(promises).then(function (result) {
+                            resolve([].concat.apply([], result));
+                        });
+                    });
+                });
+            };
+
+            trace(absolutePath, []).then(function (result) {
+                var size = _lodash2.default.sumBy(result, function (obj) {
+                    return obj.size * 1;
+                });
+
+                resolve({
+                    size: size.toFixed(2),
+                    data: result
+                });
             });
         };
-
-        trace(absolutePath, []).then(function (result) {
-            var size = _lodash2.default.sumBy(result, function (obj) {
-                return obj.size * 1;
-            });
-
-            callback({
-                size: size.toFixed(2),
-                data: result
-            });
-        });
     };
 };
 
